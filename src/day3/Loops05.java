ba262a9732e8236f705edc5852f2595ba7e33ede
mehmet <mfidan21@gmail.com>
2022-02-04 12:31:53 +0300

    04.22

--- src/day3/Loops05.java
+++ src/day3/Loops05.java
@@ -1,10 +1,32 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Loops05 {
 
     public static void main(String[] args) {
 
         //Take integer inputs from user until he/she presses q ( Ask to press q to quit after every integer input ).
-        // Print product of all numbers.
+        // Print sum of all numbers.
+
+        Scanner scann = new Scanner(System.in);
+
+
+        String secim;
+        int sum=0 ;
+
+
+       do {
+           System.out.println("enter a number and for quit enter q");
+           secim = scann.nextLine();
+
+           if (!secim.equalsIgnoreCase("q")) {
+               int number = Integer.parseInt(secim);
+               sum += number;
+
+           }
+       } while (!secim.equalsIgnoreCase("q")) ;
+
+        System.out.print("sum of numbers: " + sum);
     }
 }
